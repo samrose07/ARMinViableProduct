c0dce4571b47c95fb40e800ae715f3879f056ffc
meganbrady1 <46699185+meganbrady1@users.noreply.github.com>
2020-04-23 00:05:09 -0600

    hat menu
    Added the hat menu to the AR scene

---
+++ UItest/Assets/Scripts/HatMenuHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HatMenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject hatMenu;
+	void Start () {
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(MenuClicked);
+	}
+	
+	// Update is called once per frame
+	void MenuClicked () {
+		if (!hatMenu.activeInHierarchy) hatMenu.SetActive(true);                         
+        else if (hatMenu.activeInHierarchy) hatMenu.SetActive(false);
+	}
+}
